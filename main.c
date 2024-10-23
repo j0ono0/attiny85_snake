64dbb68a8ffa6ee423fd19e970b71feffffc051f
John Newall <john@johnnewall.com>
2024-10-23 20:19:17 +1030

    success box animation with mini-buffer

--- main.c
+++ main.c
@@ -45,8 +45,10 @@ void draw_box(uint8_t x, uint8_t y)
 }
 
 typedef struct box {
-    uint8_t x;
-    uint8_t y;
+    uint8_t x1;
+    uint8_t x2;
+    uint8_t y1;
+    uint8_t y2;
 } box;
 
 
@@ -59,8 +61,11 @@ int main()
     CLKPR=0;
     //==================================================
 
+    // IDEA: space saver - upper nibble as x1, lower nibble as x2
 
-    // box b1 = {.x=0, .y=0};
+    box b1 = {.x1=0, .x2=7, .y1=4, .y2=11};
+    
+    uint8_t dy = 1;
 
 	i2c_init();
 	ssd1306_init();
@@ -68,51 +73,45 @@ int main()
 
     clear_screen();
 
-    // Draw a box
-    set_column_address(8, 15);
-	set_page_address(1, 1);
-    ssd1306_start();
-    i2c_write_byte(SSD1306_CONTROL_BYTE_DATA);
-    for(int i = 0; i < 8; ++i)
-    {
-        i2c_write_byte(0xFF<<3);
-    }
-    ssd1306_stop();
-    // Draw a box bottom
-    set_column_address(8, 15);
-	set_page_address(2, 2);
-    ssd1306_start();
-    i2c_write_byte(SSD1306_CONTROL_BYTE_DATA);
-    for(int i = 0; i < 8; ++i)
-    {
-        i2c_write_byte(0xFF>>5);
-    }
-    ssd1306_stop();
-
-
-
 
 	// ssd1306_send_progmem_data(default_image_length, image_2); // ssd1306 raw example: show an image
     // _delay_ms(1000);
 
-    // uint8_t x_dir = 1;
 
     while(1)
     {
-        // clear_screen();
-        // draw_box(b2.x, b2.y);
-        // draw_box(b1.x, b1.y);
-        // _delay_ms(250);
-        // b1.x += x_dir;
-        // if(b1.x == 15){
-        //     x_dir = -1;
-        // }else if(b1.x == 0)
-        // {
-        //     x_dir = 1;
-        // }
+    ssd1306_start();
+    i2c_write_byte(SSD1306_CONTROL_BYTE_DATA);
+        
+        for(uint8_t page = 0; page < 8; ++page)
+        {
+            for(uint8_t col = 0; col < 128; ++col)
+            {
+                uint8_t bytebuffer = 0x00;
+                for(uint8_t bit = 0; bit < 8; ++bit)
+                {
+                    uint8_t x = col;
+                    uint8_t y = page * 8 + bit;
+                    if(x >= b1.x1 && x <= b1.x2 && y >= b1.y1 && y <= b1.y2)
+                    {
+                        bytebuffer |= (1 << bit);
+                    }
+                }
+            i2c_write_byte(bytebuffer);
 
-
-
+            }
+        }
+    ssd1306_stop();
+    b1.y1 += dy;
+    b1.y2 += dy;
+    if(b1.y1 == 0)
+    {
+        dy = 1;
+    }
+    else if(b1.y2 == 63){
+        dy = -1;
+    }
+        _delay_ms(0);
     }
     
     return 0;
