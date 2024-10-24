749a90803b08a84c4e9d12f14226083db3a83c36
John Newall <john@johnnewall.com>
2024-10-24 13:53:44 +1030

    tidy up / typos

--- t85_adc.c
+++ t85_adc.c
@@ -9,9 +9,7 @@ void init_adc()
 
     
     ADMUX |= (1 << ADLAR) |             // Left adjust result (8bit accuracy)
-             (1 << MUX1) | (1 << MUX0); //  Analog Channe: PB3
-    
-    // ADC clock must run between 50kHz and 200KHz. Main clock running at 8MHz
+             (1 << MUX1) | (1 << MUX0); //  Analog Channel: PB3
     
     ADCSRA |= (1 << ADEN) |                 // ADC Enable
               (1 << ADPS2) | (1 << ADPS1); // Prescale divide factor = 64
@@ -20,12 +18,7 @@ void init_adc()
 
 uint8_t read_adc()
 {
-    ADCSRA |= (1 << ADSC); // Start Conversion
-
-    // Wait for conversion to complete
-    while(!(ADCSRA & (1 << ADSC)));
-
-    // Read ADCH register for result
-    uint8_t result = ADCH;
-    return result;
+    ADCSRA |= (1 << ADSC);          // Start Conversion
+    while(!(ADCSRA & (1 << ADSC))); // Wait for conversion to complete
+    return ADCH;
 }
